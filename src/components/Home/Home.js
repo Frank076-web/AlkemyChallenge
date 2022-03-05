be67eb00eee25e3fd289286e2f1feefefa9905ce
Frank076 <franco.776@hotmail.com>
2022-03-05 19:02:06 -0300

    fixing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import HeroCard from './HeroCard';
 import './Home.css';
 import LogOut from '../logOut/LogOut';
 import '../logOut/LogOut.css';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
 	const [heroId, setHeroId] = useState([]);
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,7 +6,7 @@ import HeroCard from './HeroCard';
 import './Search.css';
 import LogOut from '../logOut/LogOut';
 import '../logOut/LogOut.css';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function SearchHero() {
 	//Manejo del formulario
